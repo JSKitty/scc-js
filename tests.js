432dc97e07fbf0872e2cde7a37707f32767df98b
JSKitty <loopylew113@protonmail.com>
2021-06-08 02:36:44 +0100

    [Tests] Perform multiple derivations for Scalar testing
    Noble secp256k1 uses scalar multiplication with precomputation, this slows down the first derivation by about 20x, but future calls should be sped up largely by this, as demonstrated in the 10x derivation test.

--- tests.js
+++ tests.js
@@ -12,10 +12,13 @@ async function tests () {
     console.log("Pub:  " + cWallet.pubkey);
     console.log("Test 1 --- End (took " + (Date.now() - nStartTime) + " ms)\n\n");
 
-    console.log("Test 2 --- Derive Pubkey from WIF Privkey");
+    console.log("Test 2 --- Derive 10 Pubkeys from WIF Privkey");
     nStartTime = Date.now();
-    let strPubkey = await scc.pubFromPriv(cWallet.privkey);
-    console.log("Pub:  " + strPubkey);
+    for (let i=0; i<10; i++) {
+        let nKeyStartTime = Date.now();
+        let strPubkey = await scc.pubFromPriv(cWallet.privkey);
+        console.log("Pub:  " + strPubkey + ", took " + (Date.now() - nKeyStartTime) + " ms");
+    }
     console.log("Test 2 --- End (took " + (Date.now() - nStartTime) + " ms)");
 }
 
