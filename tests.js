f27b2e2d21095649331f616f1a13a6188a86da90
Luke <luke@lukehanslarsen.com>
2021-06-14 04:12:10 -0400

    Transaction fixes

--- tests.js
+++ tests.js
@@ -1,5 +1,5 @@
 // Require our modules
-var wallet = require('./scripts/wallet.js');
+var scc = require('./index.js');
 
 async function tests () {
     /* WALLET TESTS */
@@ -7,15 +7,18 @@ async function tests () {
     // Wallet generation
     console.log("Test 1 --- Wallet Generation");
     let nStartTime = Date.now();
-    let cWallet = await wallet.generateWallet();
+    let cWallet = await scc.generateWallet();
     console.log("Priv: " + cWallet.privkey);
     console.log("Pub:  " + cWallet.pubkey);
     console.log("Test 1 --- End (took " + (Date.now() - nStartTime) + " ms)\n\n");
 
-    console.log("Test 2 --- Derive Pubkey from WIF Privkey");
+    console.log("Test 2 --- Derive 10 Pubkeys from WIF Privkey");
     nStartTime = Date.now();
-    let strPubkey = await wallet.pubFromPriv(cWallet.privkey);
-    console.log("Pub:  " + strPubkey);
+    for (let i=0; i<10; i++) {
+        let nKeyStartTime = Date.now();
+        let strPubkey = await scc.pubFromPriv(cWallet.privkey);
+        console.log("Pub:  " + strPubkey + ", took " + (Date.now() - nKeyStartTime) + " ms");
+    }
     console.log("Test 2 --- End (took " + (Date.now() - nStartTime) + " ms)");
 }
 
