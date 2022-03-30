0db703d58a39401979933e50545796a0d399ca01
JSKitty <jskitty@protonmail.com>
2022-03-30 23:14:06 +0000

    [Trivial] Update tests to match docs style

--- tests.js
+++ tests.js
@@ -1,5 +1,5 @@
 // Require our modules
-var scc = require('./index.js');
+var SCC = require('./index.js');
 
 async function tests() {
     /* WALLET TESTS */
@@ -7,7 +7,7 @@ async function tests() {
     // Wallet generation
     console.log("Test 1 --- Wallet Generation");
     let nStartTime = Date.now();
-    const cWallet = await scc.wallet.generateWallet();
+    const cWallet = await SCC.wallet.generateWallet();
     console.log("Priv: " + cWallet.privkey);
     console.log("Pub:  " + cWallet.pubkey);
     console.log("Test 1 --- End (took " + (Date.now() - nStartTime) + " ms)\n\n");
@@ -17,7 +17,7 @@ async function tests() {
     nStartTime = Date.now();
     for (let i=0; i<10; ++i) {
         const nKeyStartTime = Date.now();
-        const strPubkey = await scc.wallet.pubFromPriv(cWallet.privkey);
+        const strPubkey = SCC.wallet.pubFromPriv(cWallet.privkey);
         console.log("Pub:  " + strPubkey + ", took " + (Date.now() - nKeyStartTime) + " ms");
     }
     console.log("Test 2 --- End (took " + (Date.now() - nStartTime) + " ms)\n\n");
@@ -26,7 +26,7 @@ async function tests() {
     console.log('Test 3 --- Create a signature using our test wallet');
 
     nStartTime = Date.now();
-    const cSig = await scc.signer.sign('test', cWallet.privkey, true, {extraEntropy: true});
+    const cSig = await SCC.signer.sign('test', cWallet.privkey, true, {extraEntropy: true});
     console.log('Sig:  ' + cSig.toString('base64'));
     console.log('Test 3 --- End (took ' + (Date.now() - nStartTime) + ' ms)\n\n');
 
@@ -34,7 +34,7 @@ async function tests() {
     console.log('Test 4 --- Verify the signature using only the sig, content and pubkey');
 
     nStartTime = Date.now();
-    const fSigVerif = await scc.signer.verify('test', cWallet.pubkey, cSig);
+    const fSigVerif = await SCC.signer.verify('test', cWallet.pubkey, cSig);
     console.log('Verification:  ' + (fSigVerif ? 'Success!' : 'Failed!'));
     console.log('Test 4 --- End (took ' + (Date.now() - nStartTime) + ' ms)');
 }
